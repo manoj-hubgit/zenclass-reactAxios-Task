e1ccf8d8f692bd069523d72cf114bdac11f02dcf
manoj-hubgit <manojannadurai2265@gmail.com>
2024-06-09 17:07:00 +0530

    Name changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-  <Link className="navbar-brand d-flex" to='/'><h2>User</h2></Link>
+  <Link className="navbar-brand d-flex" to='/'><h2>UserHub</h2></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
